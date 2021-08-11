851c2fe11c7cf2ce358bc45dd781488ba18f3b80
saarsc <scsaar@gmail.com>
2021-08-11 12:18:16 +0300

    Images readme and done

--- isaac-casino.lua
+++ isaac-casino.lua
@@ -4,7 +4,7 @@ my_settings = nil
 characters = {}
 extras = {}
 bosses = {}
-loaded = false
+
 -- Description displayed in the Scripts dialog window
 function script_description()
     return [[Isaac Casino
@@ -14,25 +14,27 @@ function script_update(settings)
     -- Keep track of current settings
     my_settings = settings
 end
+-- When script is loaded for the first time load all the images(options) into relevent arrays
 function script_load(settings)
+    -- Scanning given path and push it into given array
     function loadFilesIntoArr(arr, path)
         local dir = obs.os_opendir(script_path() .. "/Imges/" .. path)
         local entry
         repeat
             entry = obs.os_readdir(dir)
             if entry and not entry.directory then
-                print(entry.d_name)
                 table.insert(arr, string.sub(entry.d_name, 0, -5))
             end
         until not entry
     end
-    loadFilesIntoArr(characters, "Characters")
-    -- loadFilesIntoArr(extras, "Extras")
-    -- loadFilesIntoArr(bosses, "Bosses")
+    obslua.os_closedir(dir)
 
-    -- obs.obs_frontend_add_event_callback(on_event)
+    loadFilesIntoArr(characters, "Characters")
+    loadFilesIntoArr(extras, "Extras")
+    loadFilesIntoArr(bosses, "Bosses")
+
     print("Done loading")
-    loaded = true
+
 end
 
 -- Create the sources for the images
@@ -43,7 +45,6 @@ function create_image(file_location, name, scene, xpos, ypos, xscale, yscale)
 
     local image_settings = obs.obs_data_create()
     obs.obs_data_set_string(image_settings, "file", script_path() .. file_location)
-    print(file_location)
     if (obs.obs_scene_find_source(scene, name)) then
         local image_source = obs.obs_scene_find_source(scene, name)
         obs.obs_sceneitem_remove(image_source)
@@ -69,19 +70,93 @@ function create_image(file_location, name, scene, xpos, ypos, xscale, yscale)
     obs.obs_source_release(image_source)
 
 end
+-- Takes in an array retuns a table with [option] => chance structre 
+function sumChances(arr)
+    local finalChances = {}
+    local sum = 0
+    for _, val in ipairs(arr) do
+        finalChances[val] = obslua.obs_data_get_int(my_settings, val)
+        sum = sum + obslua.obs_data_get_int(my_settings, val)
+    end
+    table.sort(finalChances)
+    return finalChances, sum
+end
+
 -- Generate new run
 function button()
+    -- Selects random value from arr based on given chances
+    function generateRandom(arr, fill)
+        fill = fill or false
+        local chances, totalSum = sumChances(arr)
+        local accuSum = 0
+
+        if (fill and totalSum < 100) then
+            chances["none"] = 100 - totalSum
+            table.sort(chances)
+        end
+
+        math.randomseed(os.time())
+        local rand = math.random(totalSum)
+        for key, chance in pairs(chances) do
+            accuSum = accuSum + chance
+            if (rand <= accuSum) then
+                return key
+            end
+        end
+    end
+    -- Filters the extra options based on the path
+    function generateExtra(boss)
+        local extrasFiltered = {}
+        for _, val in ipairs(extras) do
+            if (boss == "Dogma" and val ~= "Hush") then
+                table.insert(extrasFiltered, val)
+            elseif (boss ~= "Dogma") then
+                table.insert(extrasFiltered, val)
+            end
+        end
+        return generateRandom(extrasFiltered, true)
+    end
 
     local current_scene = obs.obs_frontend_get_current_scene()
     local scene = obs.obs_scene_from_source(current_scene)
     obs.obs_scene_release(scene)
 
-    create_image("Imges\\Result\\character.png", "Character", scene, 0, 0, 10, 10)
-end
+    local char = generateRandom(characters)
+    print(char)
+    local boss = generateRandom(bosses)
+    print(boss)
 
+    local extra = generateExtra(boss)
+    print(extra)
+    local xpos = obslua.obs_data_get_int(my_settings, "xpos")
+    local ypos = obslua.obs_data_get_int(my_settings, "ypos")
+    local xscale = obslua.obs_data_get_int(my_settings, "xscale")
+    local yscale = obslua.obs_data_get_int(my_settings, "yscale")
+
+    local xoffset = 60 * xscale
+    create_image("Imges\\Characters\\" .. char .. ".png", "Character", scene, xpos, ypos, xscale, yscale)
+    xpos = xpos + xoffset
+    create_image("Imges\\arrow.png", "fArrow", scene, xpos, ypos, xscale, yscale)
+    xpos = xpos + xoffset
+    if (extra ~= "none") then
+        create_image("Imges\\Extras\\" .. extra .. ".png", "Extra", scene, xpos, ypos, xscale, yscale)
+        xpos = xpos + xoffset
+        create_image("Imges\\arrow.png", "sArrow", scene, xpos, ypos, xscale, yscale)
+        xpos = xpos + xoffset
+    else
+        local image_source = obs.obs_scene_find_source(scene, "sArrow")
+        obs.obs_sceneitem_remove(image_source)
+        image_source = obs.obs_scene_find_source(scene, "Extra")
+        obs.obs_sceneitem_remove(image_source)
+    end
+    create_image("Imges\\Bosses\\" .. boss .. ".png", "Boss", scene, xpos, ypos, xscale, yscale)
+
+end
+-- Show all properties
 function script_properties()
 
     function generateProperties(arr, properties)
+        -- Generating the properties baesd on a given array
         for _, val in ipairs(arr) do
             obslua.obs_properties_add_int(properties, val, "Chances of " .. val .. " (%)", 0, 100, 1)
         end
@@ -89,7 +164,7 @@ function script_properties()
     local properties = obs.obs_properties_create()
 
     -- Combo list filled with the options from MY_OPTIONS
-    local p = obslua.obs_properties_add_list(properties, "mode", "My list", obslua.OBS_COMBO_TYPE_LIST,
+    local p = obslua.obs_properties_add_list(properties, "mode", "Mode", obslua.OBS_COMBO_TYPE_LIST,
         obslua.OBS_COMBO_FORMAT_INT)
     MY_OPTIONS = {"Characters", "Extras", "Final Bosses"}
     for i, v in ipairs(MY_OPTIONS) do
@@ -100,18 +175,20 @@ function script_properties()
     obslua.obs_property_set_modified_callback(p, set_visibility)
 
     -- Integer option to be displayed in Mode 1
-    if (loaded == true) then
-        print("props gen")
-        generateProperties(characters, properties)
-    end
+
+    generateProperties(characters, properties)
+    generateProperties(extras, properties)
+    generateProperties(bosses, properties)
 
     -- Calls the callback once to set-up current visibility
     obslua.obs_properties_apply_settings(properties, my_settings)
-    -- Generate Run
-    obslua.obs_properties_add_int(properties, "xpos", "X location start", 0, 4000, 10)
-    obslua.obs_properties_add_int(properties, "ypos", "X location start", 0, 4000, 10)
-    obslua.obs_properties_add_int(properties, "xscale", "x size", 0, 100, 10)
-    obslua.obs_properties_add_int(properties, "yscale", "y size", 0, 100, 10)
+
+    -- Base props
+    obslua.obs_properties_add_float(properties, "xpos", "X location start", 0, 4000, 10)
+    obslua.obs_properties_add_float(properties, "ypos", "X location start", 0, 4000, 10)
+    obslua.obs_properties_add_float(properties, "xscale", "x size", 0, 100, 10)
+    obslua.obs_properties_add_float(properties, "yscale", "y size", 0, 100, 10)
+    obslua.obs_hotkey_register_frontend("isaaccasinonewrun", "Generates a new Isaac path", button)
     local button = obs.obs_properties_add_button(properties, "generate_run", "Generate Run", button)
     return properties
 
